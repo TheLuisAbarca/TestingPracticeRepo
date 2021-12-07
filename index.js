66fc69dbd85c993607723b25d67666b0f4b29034
TheLuisAbarca <arclaav.adm@gmail.com>
2021-12-07 16:31:57 -0500

    Add a Calculator class with 4 basic operations, write 3 tests for each operation method and separate with describe into tests.

--- index.js
+++ index.js
@@ -14,4 +14,68 @@ function reverseString(string) {
   }
 }
 
-module.exports = {stringLength, reverseString};
+class Calculator {
+  constructor(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+
+  checkParameterExists() {
+    if (this.num1 && this.num2) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+
+  checkParametersType() {
+    if (typeof this.num1 === "number" && typeof this.num2 === "number") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  add() {
+    if (!this.checkParameterExists()) {
+      return "Parameters are missing.";
+    }
+    if (!this.checkParametersType()) {
+      return "Parameters must be numbers.";
+    }
+    return this.num1 + this.num2;
+  }
+
+  subtract() {
+    if (!this.checkParameterExists()) {
+      return "Parameters are missing.";
+    }
+    if (!this.checkParametersType()) {
+      return "Parameters must be numbers.";
+    }
+    return this.num1 - this.num2;
+  }
+
+  multiply() {
+    if (!this.checkParameterExists()) {
+        return "Parameters are missing.";
+    }
+    if (!this.checkParametersType()) {
+      return "Parameters must be numbers.";
+    }
+    return this.num1 * this.num2;
+  }
+
+  divide() {
+    if (!this.checkParameterExists()) {
+      return "Parameters are missing.";
+    }
+    if (!this.checkParametersType()) {
+      return "Parameters must be numbers.";
+    }
+    return this.num1 / this.num2;
+  }
+}
+const num1 = new Calculator(10, 5);
+console.log(num1.add());
+module.exports = {stringLength, reverseString, Calculator};
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const {stringLength, reverseString} = require('./index');
+const {stringLength, reverseString, Calculator} = require('./index');
 
 test('lol has 3 characters as string length.', () => {
     expect(stringLength('lol')).toBe(3);
@@ -22,4 +22,66 @@ test('Empty has 0 characters as string length.', () => {
 
 test('Murcielagos has 11 characters as string length.', () => {
     expect(reverseString('Murcielagos')).toMatch(/Invalid string/);
+});
+
+describe('Calculator', () => {
+    const Tupla = new Calculator(10,5);
+    const TuplaWrongParameters = new Calculator('',5);
+    const TuplaString = new Calculator('Paloma',5);
+
+    describe('Addition', () => {
+        test('One parameter is empty for Sum.', () => {
+            expect(TuplaWrongParameters.add()).toMatch(/Parameters are missing./);
+        });
+
+        test('One parameter is string for Sum.', () => {
+            expect(TuplaString.add()).toMatch(/Parameters must be numbers./);
+        });
+
+        test('Sum of 10 and 5 is 15.', () => {
+            expect(Tupla.add()).toBe(15);
+        });
+    });
+
+    describe('Subtraction', () => {
+        test('One parameter is empty for subtraction.', () => {
+            expect(TuplaWrongParameters.subtract()).toMatch(/Parameters are missing./);
+        });
+
+        test('One parameter is string for subtraction.', () => {
+            expect(TuplaString.subtract()).toMatch(/Parameters must be numbers./);
+        });
+
+        test('Subtraction of 10 minus 5 is 5.', () => {
+            expect(Tupla.subtract()).toBe(5);
+        });
+    });
+
+    describe('Multiplication', () => {
+        test('One parameter is empty for multiplication.', () => {
+            expect(TuplaWrongParameters.multiply()).toMatch(/Parameters are missing./);
+        });
+    
+        test('One parameter is string for multiplication.', () => {
+            expect(TuplaString.multiply()).toMatch(/Parameters must be numbers./);
+        });
+    
+        test('Multiplication of 10 times 5 is 50.', () => {
+            expect(Tupla.multiply()).toBe(50);
+        });
+    });
+
+    describe('Division', () => {
+        test('One parameter is empty for division.', () => {
+            expect(TuplaWrongParameters.divide()).toMatch(/Parameters are missing./);
+        });
+    
+        test('One parameter is string for division.', () => {
+            expect(TuplaString.divide()).toMatch(/Parameters must be numbers./);
+        });
+    
+        test('Division of 10 divides 5 is 2.', () => {
+            expect(Tupla.divide()).toBe(2);
+        });
+    });
 });
