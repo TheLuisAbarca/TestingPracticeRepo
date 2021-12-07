6e430cf1ef8bf5186b74ee2573ad8b28f2824ebe
TheLuisAbarca <arclaav.adm@gmail.com>
2021-12-07 15:10:34 -0500

    Add validation in which the string has to be at least 1 character long and not longer than 10 characters, Write test to that validation.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function stringLength(string) {
-  return string.length;
+  if (string.length > 0 && string.length < 10) {
+    return string.length;
+  } else {
+    return "Invalid string";
+  }
 }
 
 module.exports = stringLength;
--- test.js
+++ test.js
@@ -3,3 +3,11 @@ const funStringLength = require('./index');
 test('lol has 3 characters as string length.', () => {
     expect(funStringLength('lol')).toBe(3);
 });
+
+test('Empty has 0 characters as string length.', () => {
+    expect(funStringLength('')).toMatch(/Invalid string/);
+});
+
+test('Murcielagos has 11 characters as string length.', () => {
+    expect(funStringLength('Murcielagos')).toMatch(/Invalid string/);
+});
